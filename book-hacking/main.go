9b128f4a0b257bf54f14d1a3a4d863acabccb1fa
danny crasto <danwald79@gmail.com>
2018-12-17 20:19:18 +0400

    fixes whitespace in hack code

--- book-hacking/main.go
+++ book-hacking/main.go
@@ -38,14 +38,14 @@ func main(){
     elements := map[string]map[string]string {
 		"H": {"name": "Hydrogen", "state": "gas", },
 		"He": {"name": "Helium", "state": "gas", },
-    	"Li": {"name": "Lithium", "state": "solid", },
-    	"Be": {"name": "Beryllium", "state": "solid", },
-    	"B": {"name": "Boron", "state": "solid", },
-    	"C": {"name": "Carbon", "state": "solid", },
-    	"N": {"name": "Nitrogen", "state": "gas", },
-    	"O": {"name": "Oxygen", "state": "gas", },
-    	"F": {"name": "Fluorine", "state": "gas", },
-    	"Ne": {"name": "Neon", "state": "gas", },
+        "Li": {"name": "Lithium", "state": "solid", },
+        "Be": {"name": "Beryllium", "state": "solid", },
+        "B": {"name": "Boron", "state": "solid", },
+        "C": {"name": "Carbon", "state": "solid", },
+        "N": {"name": "Nitrogen", "state": "gas", },
+        "O": {"name": "Oxygen", "state": "gas", },
+        "F": {"name": "Fluorine", "state": "gas", },
+        "Ne": {"name": "Neon", "state": "gas", },
     }
 	fmt.Println("Map:", elements, "\nEnter a key:")
 	var sin string
